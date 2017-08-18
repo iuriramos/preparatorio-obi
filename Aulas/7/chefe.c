d9e07215180d5a2349a8cb6df187daf537a3c11c
Iuri Ramos <iurisouzaramosbarbosa@gmail.com>
2017-08-18 02:34:20 -0300

    Update chefe.c
    A implementação que fizemos em sala de aula não está completa. Vou atualizar o código python depois.
    Com as mudanças que fiz agora o seu código está funcionando perfeitamente. Estava atualizando o vetor visitado incorretamente (atualizando row quando o certo é atualizar col), estava trocando idades (não precisa) e faltava trocar as colunas da matriz de adj. nas operações de troca (somente as linhas estavam sendo trocadas). Depois dá uma olhada para ver se consegue entender tudo, se restar alguma dúvida é só me falar. Abraço!

--- Aulas/7/chefe.c
+++ Aulas/7/chefe.c
@@ -2,46 +2,56 @@
 #define true 1
 #define false 0
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
-int a, idade;
-int _min_idade(int row, int adj[510][510], int num, int Idade[510], int visitado[])
+
+int _min_idade(int row, int adj[510][510], int N, int idades[510], int visitado[510])
 {
-    int minimo;
-    minimo = Idade[row];
-    for(a=1;a<=num;a++)//a eh a coluna
+    // printf("start -- min idade (%d)\n", row);
+    visitado[row]=true; // visita nó assim que entra na função
+    int minimo = idades[row];
+    for(int col=1; col<=N; col++)//a eh a coluna
     {
-        minimo = Idade[row];
-        if (adj[row][a]==1&&visitado[row]==false)//se ele tem um chefe e se ele nao foi visitado
+        if (adj[row][col]==1 && visitado[col]==false)//se ele tem um chefe e se ele nao foi visitado
         {
-            visitado[row]=true;
-            idade=_min_idade(a,adj,num,Idade,visitado);//agora voce ve se o chefe tem outro chefe para achar o chefe indireto
+            // não eh mais necessario, agora nó é marcado assim que chega na função
+            // visitado[col]=true;
+            int idade=_min_idade(col,adj,N,idades,visitado);//agora voce ve se o chefe tem outro chefe para achar o chefe indireto
             minimo=MIN(minimo,idade);
         }
     }
-    visitado[row]=true;
+    // printf("end -- min idade (%d): %d\n", row, minimo);
     return minimo;
 }
-int min_idade(int row1, int Adj1[510][510], int num1, int idade1[510])
-{
-    int minimo1;
-    int visitado1[510];
-    for(a=1;a<=num1;a++)
-    {
-        visitado1[a]=false;
+void print_adj(int adj[510][510], int N) {
+    printf("[\n");
+    for (int i = 1; i <= N; i++) {
+        printf("[ ");
+        for (int j = 1; j <= N; j++) {
+            printf("%d, ", adj[i][j]);
+        }
+        printf("]\n");
     }
-    minimo1=101;
-    for(a=1;a>=num1;a++)
+    printf("]\n");
+}
+int min_idade(int row, int adj[510][510], int N, int idades[510])
+{
+    int minimo = 101;
+    int visitado[510];
+    for(int a=1;a<=N;a++)
     {
-        if(Adj1[row1][a]==1)
+        visitado[a]=false;
+    }
+    for(int col=1;col<=N;col++)
+    {
+        if(adj[row][col]==1 && visitado[col]==false)
         {
-            idade = _min_idade(row1,Adj1,num1,idade1,visitado1);
-            minimo1 = MIN(minimo1,idade);
+            int idade = _min_idade(col, adj, N, idades, visitado);
+            minimo = MIN(minimo, idade);
         }
     }
-    if(minimo1<=100)
+    if(minimo <= 100)
     {
-        return minimo1;
+        return minimo;
     }
-    printf("%d",visitado1[row1]);
     return 101;
 }
 int main()
@@ -60,6 +70,7 @@ int main()
         scanf("%d %d",&y,&x);
         adjm[x][y]=1;
     }
+    // print_adj(adjm, N);
     getchar();
     for(i=1;i<=I;i++)
     {
@@ -81,16 +92,24 @@ int main()
         else if(comando=='T')
         {
             scanf("%d %d",&k, &j);
-            temp=idadem[k];
-            idadem[k]=idadem[j];
-            idadem[j]=temp;
+            // temp=idadem[k];
+            // idadem[k]=idadem[j];
+            // idadem[j]=temp;
             for(l=1;l<=N;l++)
             {
                 aux[l]=adjm[k][l];
                 adjm[k][l]=adjm[j][l];
                 adjm[j][l]=aux[l];
             }
+            // colunas também, vértices devem atualizar os locais para onde apontam
+            for(l=1;l<=N;l++)
+            {
+                aux[l]=adjm[l][k];
+                adjm[l][k]=adjm[l][j];
+                adjm[l][j]=aux[l];
+            }
             getchar();
+            // print_adj(adjm, N);
         }
     }
 }
