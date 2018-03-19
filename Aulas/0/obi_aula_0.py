25a63c569d8a318e1fe3fd9d50b34ebe3022c642
iuriramos <iuri.srb@gmail.com>
2018-03-19 17:00:46 -0300

    Códigos executados durante a primeira aula da OBI

---
+++ Aulas/0/obi_aula_0.py
@@ -0,0 +1,97 @@
+# coding: utf-8
+
+10 + 20
+20 - 10
+type(10)
+10.2
+type(10.2)
+'Ola mundo'
+type('Ola mundo')
+10 * 8
+10 ** 8
+10 / 2
+type(10 / 2)
+int(10 / 2)
+10 // 2
+int('10')
+int(10.0)
+float(10)
+float('10.0')
+float('Olá')
+float('10.0 alsdkf')
+float('10.0 ')
+float('            10.0    ')
+10 //0
+str(10)
+str(10.0)
+sqrt(100)
+print(10)
+print ('Olá mundo' + ' ...')
+print ('Olá mundo' + 10)
+help(str)
+str(10)
+'10'
+help(max)
+max(1, 2)
+max(1, 2, 3, 4, 5, 6)
+max([1, 2, 3, 4, 5])
+min(1, 2, 3)
+# |1-2|
+abs(1-2)
+# mod
+22 % 10
+22 // 10
+29 // 10
+help(divmod)
+divmod(29, 10)
+import math
+help(math)
+help(math.raiz)
+help(math.sqrt)
+math.sqrt(100)
+math.sqrt(110)
+int(math.sqrt(100))
+math.pi
+math.sin(math.pi)
+math.sin(math.pi/2)
+from math import sqrt
+math.sqrt(100)
+sqrt(100)
+100**(0.5)
+100**(1/2)
+number1 = 10
+number2 = 2
+number1**(1//number2)
+number1**(1/number2)
+help(math)
+help(math.factorial
+)
+math.factorial(10)
+math.factorial(5)
+a = input('Escolha um número de 1 a 10')
+a = input('Escolha um número de 1 a 10: ')
+a
+type(a)
+a = int(a)
+a
+a = int(input('Escolha.....: '))
+a
+a
+a + a
+a * a
+input('Usuário digite um número: ')
+a = input('Usuário digite um número: ')
+a
+a = int(a)
+a
+type(a)
+a
+b
+b = 10
+a
+b
+a + b
+'Olá ' + 'Mundo'
+'Olá ' * 8
+'*' * 8
+'*' * -8
