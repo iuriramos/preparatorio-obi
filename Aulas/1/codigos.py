532b0f5ffc9cb48798e0cf77a15fd678cf1c34c9
iuriramos <iuri.srb@gmail.com>
2018-03-21 00:44:33 -0300

    Atualização trechos de código aula 1

---
+++ Aulas/1/codigos.py
@@ -0,0 +1,190 @@
+# coding: utf-8
+5
+type(5)
+5.3
+type(5.3)
+'Olá mundo'
+type('Olá mundo')
+complex(2, 10)
+5 + 5
+5 * 8
+(5 + 4) * 3
+2 ** 3 ** 2
+2 ** (3 ** 2)
+2 ** 9
+(2 ** 3) ** 2
+5
+6 + 3
+a = 9
+a + 6
+a + 15
+a * 7
+print(a)
+print ('Olá mundo')
+msg = 'Olá mundo'
+print (msg)
+type(msg)
+2 + 5
+2 + '5'
+4 + 2.0
+type(2.0)
+int('5')
+int('5      ')
+str(5)
+str(5.0)
+float(2)
+float('2')
+msg
+'olá mundo' + ' !!!'
+msg + ' !!!'
+msg + msg
+'ola' * 'mundo'
+True
+type(True)
+False
+type(False)
+True and True
+True and False
+False and True
+False and False
+True or True
+True or False
+False or False
+False or TRue
+False or True
+=, +, -, *, /, //, %, ==
+a = 5
+a == 5
+a == 5.
+a == '5'
+a != 6
+a != 5
+# ==, !=, <, >, <=, >=
+a
+a < 7
+a <= 7
+a > 7
+if a == 5:
+    print('valor de a é igual a 5')
+    
+if a != 5:
+    print('valor de a é igual a 5')
+    
+b = 10
+if b < 20:
+    b = b + 10
+    
+b
+if b < 20:
+    b = b + 10
+    
+b
+for i in [1, 2, 3, 4, 5]:
+    print (i)
+    
+for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    print(i)
+    
+for i in 1, 2, 3, 4, 5, 6, 7, 8, 9, 10:
+    print(i)
+    
+    
+type([1, 2, 3]) 
+type([1, 2, 3, 'texto']) 
+type((1, 2, 3, 'texto')) 
+for i in [1, 2, 3, 4, 5]:
+    print (i)
+    
+range(100) # 0, 1, 2, ..., 100
+for i in range(100):
+    print(i)
+    
+range(100) # 0, 1, 2, ..., 99
+range(50, 101) # 50, 51, ..., 100
+for i in range(90, 101):
+    print(i)
+    
+for i in range(90, 101):
+    print(i)
+    
+# [90, 101) == [90, 100]
+range(0, 10, 2)
+for i in range(0, 10, 2):
+    print (i)
+    
+for i in range(0, 11, 2):
+    print (i)
+    
+    
+for i in range(1, 10, 2):
+    print (i)
+    
+for i in range(1, 11, 2):
+    print (i)
+    
+    
+for i in range(1, 12, 2):
+    print (i)
+    
+    
+for i in range(0, 100, 4):
+    print (i)
+    
+    
+for i in range(0, 100 + 1, 4):
+    print (i)
+    
+    
+30 % 2
+n = 64
+n % 2 == 0
+n = 63
+n % 2 == 0
+for i in range(0, 20): # 0, 1, 2,...
+    if i % 2 == 0:
+        print (i, ' é par')
+    if i % 2 != 0:
+        print (i, ' é ímpar')
+        
+    
+for i in range(0, 20): # 0, 1, 2,...
+    if i % 2 == 0:
+        print (i, ' é par')
+    if i % 2 != 0:
+        print (i, ' é ímpar')
+        
+    
+for i in range(0, 20): # 0, 1, 2,...
+    if i % 2 == 0:
+        print (i, ' é par')
+    if i % 2 != 0:
+        print (i, ' é ímpar')
+        
+    
+for i in range(100):
+    if i % 3 == 0:
+        print (i)
+        
+numero = 25
+if numero > 10 and numero % 2 == 1:
+    print (numero)
+    
+numero = 9
+if numero > 10 and numero % 2 == 1:
+    print (numero)
+    
+numero > 10
+numero % 2 == 1
+False and True
+if numero > 10 and numero % 2 == 1:
+    print (numero)
+else:
+    print ('não é válido')
+    
+for i in range(100): # 10 < x < 25 e x é par
+    if 10 < i < 25 and i % 2 == 0:
+        print (i)
+        
+for i in range(12, 25, 2): # 10 < x < 25 e x é par
+    print (i)
+
---
+++ Aulas/1/easter_egg.py
@@ -0,0 +1,10 @@
+import turtle
+
+painter = turtle.Turtle()
+painter.speed(100)
+painter.pencolor('red')
+for i in range(100):
+  painter.forward(300)
+  painter.left(123)
+
+turtle.done()
